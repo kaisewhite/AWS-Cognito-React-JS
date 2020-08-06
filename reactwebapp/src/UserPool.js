2cf43e70a34dae3b17e30b7d037c9a9867d6d886
kaisewhite <53411362+kaisewhite@users.noreply.github.com>
2020-08-06 10:24:17 -0400

    Update UserPool.js

--- reactwebapp/src/UserPool.js
+++ reactwebapp/src/UserPool.js
@@ -2,8 +2,8 @@ import { CognitoUserPool } from "amazon-cognito-identity-js";
 // Create an object that holds some information about our user pool
 // Found in the dashboard
 const poolData = {
-  UserPoolId: "us-east-1_rohQSn5VC",
-  ClientId: "7kjdf8jsdbm50rp5n0r6lrhni0" // App Client ID in Cognito -> App Clients
+  UserPoolId: "?????????????????",
+  ClientId: "????????????????" // App Client ID in Cognito -> App Clients
 };
 
 export default new CognitoUserPool(poolData);
